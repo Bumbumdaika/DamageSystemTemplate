b455d3ceda30d6cbcfdf8ab773c86baf45656e53
Bumdai <vanaslepcov861@gmail.com>
2026-03-11 00:30:53 +1000

    add: AIC_EnemyController.h, AIC_EnemyController.cpp

---
+++ Source/DamageSystemTemplate/Public/AIC_EnemyController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIC_EnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAMAGESYSTEMTEMPLATE_API AAIC_EnemyController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
